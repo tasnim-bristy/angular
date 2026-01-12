ed261728c27da4dcc72332f8dabd929e3728b994
tasnim-bristy <tasnimbristy60@gmail.com>
2026-01-12 18:28:20 +0600

    update file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
+import { Profiles } from './profiles/profiles';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'about', component:About},
+    {path:'contact', component:Contact},
+    {path:'login', component:Login},
+    {path:'home', component:Home},
+    {path:'profiles', component:Profiles},
+    {path:'**', component:PageNotFound}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Profiles } from './profiles/profiles';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { email } from '@angular/forms/signals';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Profiles],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -99,16 +102,138 @@ export class App {
   //   this.color=(event.target as HTMLInputElement).value;
   // }
 
-  users=["Oreo", "Leo", "Casper", "Nimpu", "Kaltu", "Fiona", "Laltu", "Laddu", "Liona"];
-  students=[
-    {name:'Tasnim', age:21, email:'tasnim@gmail.com'},
-    {name:'Bristy', age:20, email:'bristy@gmail.com'},
-    {name:'Mushkan', age:24, email:'mushkan@gmail.com'},
-    {name:'Nargis', age:28, email:'nargis@gmail.com'}
-  ]
+  // users=["Oreo", "Leo", "Casper", "Nimpu", "Kaltu", "Fiona", "Laltu", "Laddu", "Liona"];
+  // students=[
+  //   {name:'Tasnim', age:21, email:'tasnim@gmail.com'},
+  //   {name:'Bristy', age:20, email:'bristy@gmail.com'},
+  //   {name:'Mushkan', age:24, email:'mushkan@gmail.com'},
+  //   {name:'Nargis', age:28, email:'nargis@gmail.com'}
+  // ]
 
-  getName(name:string){
-    console.log(name);
-  }
+  // getName(name:string){
+  //   console.log(name);
+  // }
+
+  //  count=signal(10);
+  //  x=20;
+  //  constructor(){
+  //   effect(()=>{
+  //     console.log(this.count())
+  //   })
+  //  }
+  //  updateValue(val:string){
+  //   // this.count.set(this.count()+1);
+  //   if(val== 'inc'){
+  //     this.count.set(this.count()+1);
+  //   }
+  //   else{
+  //     this.count.set(this.count()-1);
+  //   }
+  //  }
+
+  // data:WritableSignal<number | string>=signal(10);
+  // count:Signal<number>=computed(()=>200);
+  // updateSignal(){
+  //   this.data.set("Hello");
+  // }
+
+  // x=signal(10);
+  // y=signal(20);
+  // z=computed(()=>this.x()+this.y());
+  // showValue(){
+  //   console.log(this.z());
+  //   this.x.set(100);
+  //   console.log(this.z())
+  // }
+  // updateX(){
+  //   this.x.set(300);
+  // }
+
+  // userName= signal('Tasnim');
+  // count=signal(0);
+  // displayHeading=false;
+  // constructor(){
+  //   effect(()=>{
+  //     if(this.count()==2){
+  //       // this.displayHeading=true;
+  //       setTimeout(()=>{
+  //         debugger;
+  //         this.displayHeading=true;
+  //         console.log(this.displayHeading);
+  //       },2000)
+  //     }
+  //     else{
+  //       this.displayHeading=false;
+  //     }
+  //   })
+  // }
+  // toggleValue(){
+  //   // this.displayHeading=!this.displayHeading; 
+  //   this.count.set(this.count()+1);
+  // }
+
+  // users=['Tasnim', 'Bristy', 'Oreo', 'Leo', 'Casper', 'laddu', 'Liona', 'Fiona', 'Laltu', 'Kaltu', 'Nimpu']
+  // users=[]
+
+  // name="tasnim";
+
+  // task="";
+  // taskList:{id:number, task:string}[]=[];
+  // addTask(){
+  //   this.taskList.push({id:this.taskList.length+1,task:this.task});
+  //   console.log(this.taskList)
+  //   this.task=''
+  // }
+  // deleteTask(taskId:number){
+  //   this.taskList=this.taskList.filter((item)=>item.id!=taskId);
+  // }
+
+  // bgColor='green';
+  // fontSize="50px";
+  // headingSizeBig='80px';
+  // headingSizeSmall='30px';
+  // zoom=false;
+  // updateHeadingSize(){
+  //   this.zoom=!this.zoom;
+  // }
+
+  // login=true;
+
+
+  // students=['anaya','amaya','mushkan'];
+  // studentsData=[
+  //   {
+  //     name:'Tasnim',
+  //     age:'21',
+  //     email:'tasnim@gmail.com'
+  //   },
+  //   {
+  //     name:'Bristy',
+  //     age:'20',
+  //     email:'bristy@gmail.com'
+  //   },
+  //   {
+  //     name:'Anaya',
+  //     age:'17',
+  //     email:'anaya@gmail.com'
+  //   },
+  //   {
+  //     name:'Amaya',
+  //     age:'16',
+  //     email:'tasnim@gmail.com'
+  //   },
+  //   {
+  //     name:'Mushkan',
+  //     age:'26',
+  //     email:'mushkan@gmail.com'
+  //   },
+  // ]
+
+  // color="black";
+  // changeColor(color:string){
+  //   this.color=color;
+  // }
+
+
 }
  
